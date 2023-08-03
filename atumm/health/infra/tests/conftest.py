f76e8c36254bf6fe5d091baa3229bdb6e601c65c
Omar <omar@atumm.org>
2023-08-03 15:56:46 +0000

    remove hypothesis

--- atumm/health/infra/tests/conftest.py
+++ atumm/health/infra/tests/conftest.py
@@ -1,16 +1,9 @@
-import asyncio
 from typing import AsyncIterator
 
 import httpx
 import pytest
-from beanie import init_beanie
-from hypothesis import settings
-from mock import Mock
-from mongomock_motor import AsyncMongoMockClient
 
 from atumm.app.infra.app.server import app
-from atumm.app.infra.injector import injector
-from atumm.user.dataproviders.beanie.models import User
 
 
 @pytest.fixture
