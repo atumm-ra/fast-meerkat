202944102d856b4aba851207cec5c0fa84aaeea2
omars44 <omar@atumm.org>
2023-10-01 10:25:44 +0300

    main: boot function

--- thisapp/main.py
+++ thisapp/main.py
@@ -1,7 +1,6 @@
 import uvicorn
 from atumm.extensions.buti.components.config import ConfigComponent
 from atumm.extensions.buti.keys import AtummContainerKeys
-from atumm.extensions.config import configure
 from atumm.extensions.fastapi.components import FastAPIComponent
 from atumm.services.health.infra.buti import HealthServiceComponent
 from atumm.services.user.infra.buti import UserServiceComponent
@@ -10,20 +9,26 @@ from buti import Bootloader, ButiStore
 from thisapp.init.buti.beanie import BeanieComponent
 from thisapp.init.buti.injector import InjectorComponent
 
-app_components = [
-    ConfigComponent(),
-    InjectorComponent(),
-    FastAPIComponent(),
-    BeanieComponent(),
-    UserServiceComponent(),
-    HealthServiceComponent(),
-]
 
-config = configure.build(".env")
-bootloader = Bootloader(app_components)
-store: ButiStore = bootloader.boot()
+def boot_app():
+    
+    app_components = [
+        ConfigComponent(),
+        InjectorComponent(),
+        FastAPIComponent(),
+        BeanieComponent(),
+        UserServiceComponent(),
+        HealthServiceComponent(),
+    ]
+    
+    bootloader = Bootloader(app_components)
+    return bootloader.boot()
+
+
+store: ButiStore = boot_app()
 
 app = store.get(AtummContainerKeys.app)
 
+# used only for debugging
 if __name__ == "__main__":
     uvicorn.run(app=app, port=5000, log_level="info")
